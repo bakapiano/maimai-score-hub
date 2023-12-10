a9bfc61a0ed96e4220be98f71884a2338e46016a
bakapiano <9417325+bakapiano@user.noreply.gitee.com>
2023-12-10 11:25:34 +0800

    fix bot

--- src/bot/net.ts
+++ src/bot/net.ts
@@ -47,13 +47,17 @@ const doFetch = async (
     fetchOptions = {
       ...options,
       body: `${options.body}&token=${token}`,
-      headers: {
-        Host: "maimai.wahlap.com",
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x6307001e)",
-        ...options.headers
-      }
     };
+  }
+
+  fetchOptions = {
+    ...fetchOptions,
+    headers: {
+      Host: "maimai.wahlap.com",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x6307001e)",
+      ...fetchOptions.headers
+    }
   }
 
   const result = await fetchWithCookieWithRetry(
